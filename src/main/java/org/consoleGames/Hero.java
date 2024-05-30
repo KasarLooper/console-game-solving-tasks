67b68458f2a03ca51508015cd485d5bc941fabd2
KasarLooper <kasar.looper@gmail.com>
2024-05-31 01:13:38 +0300

    Add main hero

---
+++ src/main/java/org/consoleGames/Hero.java
@@ -0,0 +1,13 @@
+package org.consoleGames;
+
+public class Hero {
+    private int x;
+    private int y;
+    private int hp;
+
+    private Hero(int x, int y, int hp) {
+        this.x = x;
+        this.y = y;
+        this.hp = hp;
+    }
+}
