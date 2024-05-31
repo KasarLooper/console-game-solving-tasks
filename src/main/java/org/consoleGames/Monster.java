dcd051abcc2db25051fa122d4456c0ab44918dbf
KasarLooper <kasar.looper@gmail.com>
2024-05-31 11:40:48 +0300

    Making small monster's task

--- src/main/java/org/consoleGames/Monster.java
+++ src/main/java/org/consoleGames/Monster.java
@@ -1,10 +1,15 @@
 package org.consoleGames;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Monster {
     private final int x;
     private final int y;
     private final int difficultLevel;
     private final String icon = "Мм";
+    private final Random rd = new Random();
+    private final Scanner scanner = new Scanner(System.in);
 
     public Monster(int x, int y, int difficultLevel) {
         this.x = x;
@@ -13,7 +18,20 @@ public class Monster {
     }
 
     public boolean task(int difficult) {
-        return true;
+        int x = rd.nextInt(100);
+        int y = rd.nextInt(100);
+        String task = "";
+        int writeAnswer;
+        if (rd.nextBoolean()) {
+            task = "Решите пример: %d + %d: ";
+            writeAnswer = x + y;
+        } else {
+            task = "Решите пример: %d - %d: ";
+            writeAnswer = x - y;
+        }
+        System.out.printf(task, x, y);
+        int userAnswer = scanner.nextInt();
+        return userAnswer == writeAnswer;
     }
 
     public int getX() {
