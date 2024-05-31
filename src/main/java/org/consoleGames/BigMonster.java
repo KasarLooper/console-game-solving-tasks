50c031a7f676ba913c029911ce29f1be1c2d81c9
KasarLooper <kasar.looper@gmail.com>
2024-05-31 11:49:08 +0300

    Making big monster's task

--- src/main/java/org/consoleGames/BigMonster.java
+++ src/main/java/org/consoleGames/BigMonster.java
@@ -1,7 +1,12 @@
 package org.consoleGames;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class BigMonster extends Monster {
     private final String icon = "Бм";
+    private final Random rd = new Random();
+    private final Scanner scanner = new Scanner(System.in);
 
     public BigMonster(int x, int y, int difficultLevel) {
         super(x, y, difficultLevel);
@@ -9,7 +14,30 @@ public class BigMonster extends Monster {
 
     @Override
     public boolean task(int difficult) {
-        return super.task(difficult);
+        int bound = 10 * difficult;
+        int x = rd.nextInt(bound);
+        int y = rd.nextInt(bound);
+        int z = rd.nextInt(bound);
+        int op = rd.nextInt(4);
+        String task = "";
+        int writeAnswer = 0;
+        switch (op) {
+            case 0:
+                task = "Решите пример %d + %d - %d: ";
+                writeAnswer = x + y - z;
+            case 1:
+                task = "Решите пример %d + (%d - %d): ";
+                writeAnswer = x + y - z;
+            case 2:
+                task = "Решите пример %d - (%d + %d): ";
+                writeAnswer = x - (y + z);
+            case 3:
+                task = "Решите пример %d - (%d - %d): ";
+                writeAnswer = x - (y - z);
+        }
+        System.out.printf(task, x, y, z);
+        int userAnswer = scanner.nextInt();
+        return writeAnswer == userAnswer;
     }
 
     @Override
